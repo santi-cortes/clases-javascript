5cffbf0f32125044cd9b5e023471184792b2f773
santi-cortes <santya.2003.06@gmail.com>
2024-01-31 00:48:10 -0600

    Primer ejercicio

---
+++ ejercicio1.js
@@ -0,0 +1,35 @@
+// Suma de elementos:
+//         1. Crea un arreglo de números.
+//         2. Utiliza forEach para sumar todos los elementos del array.
+//         3. Imprime el resultado.
+
+// Doble de cada elemento:
+//         1. Crea un array de números.
+//         2. Utiliza forEach para duplicar cada elemento del array.
+// 	       3. Imprime cada elemento nuevo del array.
+// 	       3.1 Imprime el nuevo arreglo modificado.
+
+//     Imprimir índice y valor:
+//         1. Crea un array de strings.
+//         2. Utiliza forEach para imprimir cada elemento junto con su indice
+
+// let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let numerosDuplicados = [];
+
+// numeros.forEach((numero) => {
+//   numerosDuplicados.push(numero * 2); // Duplica cada elemento del array
+// });
+
+// Metodos
+
+// const numerosDatos = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const persona = {
+//   nombre: "Juan",
+//   apellido: "Perez",
+//   edad: 30,
+//   saludar: function () {
+//     console.log("Hola");
+//   },
+// };
+
+// persona.saludar();
